29f70755be8390220c6d8f7b774452e1fb04b7fc
Christos Constantinides <constantinides.ch@gmail.com>
2019-04-23 16:48:07 +0200

    Update robots.js
    Updated email addresses

--- src/robots.js
+++ src/robots.js
@@ -3,60 +3,60 @@ export const robots = [
     id: 1,
     name: 'Bhrett Morgan',
     username: 'Bhrett',
-    email: 'bhrettmorgane@gmail.com'
+    email: '5894k@gmail.com'
   },
   {
     id: 2,
     name: 'Lloyd McKellar-Basset',
     username: 'Lloyd',
-    email: 'lloydcmb@icloud.com'
+    email: 'llo90@icloud.com'
   },
   {
     id: 3,
     name: 'Adam Panayiotou',
     username: 'Adam',
-    email: 'adamp@gmail.com'
+    email: 'jii9@gmail.com'
   },
   {
     id: 4,
     name: 'Kalio Charalambous',
     username: 'Kalio',
-    email: 'kalioch@gmail.com'
+    email: '990jhgg@gmail.com'
   },
   {
     id: 5,
     name: 'Constantinos Nicolaides',
     username: 'Cosi',
-    email: 'cojnic@gmail.com'
+    email: 'hh9h8@gmail.com'
   },
   {
     id: 6,
     name: 'Yaquub Salie',
     username: 'Yaquub',
-    email: 'ysalie@gmail.com'
+    email: 'dfgd54@gmail.com'
   },
   {
     id: 7,
     name: 'Christos Constantinides',
     username: 'Kiko',
-    email: 'constantinides.ch@gmail.com'
+    email: 'dfgdf4@gmail.com'
   },
   {
     id: 8,
     name: 'Alexis Toromanoff',
     username: 'Alex',
-    email: 'toromanoff@gmail.com'
+    email: 'fghfgh4@gmail.com'
   },
   {
     id: 9,
     name: 'Andrea Charalambous',
     username: 'Dre',
-    email: 'ach@gmail.com'
+    email: '5455hh@gmail.com'
   },
   {
     id: 10,
     name: 'Talya White',
     username: 'Talya',
-    email: 'twhite@gmail.com'
+    email: '1213df@gmail.com'
   }
 ];
